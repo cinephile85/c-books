a73cc2a13c542b8cc7791a8107cd692b994d4342
Justin D. Smith <cinephile85@outlook.com>
2019-02-03 18:22:03 -0500

    Fix output of exercise-07.c

--- programming-in-c/chapter-03/exercise-07/exercise-07.c
+++ programming-in-c/chapter-03/exercise-07/exercise-07.c
@@ -12,7 +12,7 @@ int main(void)
 {
     //  Evaluate the expression and display the result
     long double result = (3.31e-8L * 2.01e-7L) / (7.16e-6L + 2.01e-8L);
-    printf("%Le\n", result);
+    printf("(3.31 x 10⁻⁸ x 2.01 x 10⁻⁷) / (7.16 x 10⁻⁶ + 2.01 x 10⁻⁸) = %Le\n", result);
 
     return 0;
 }

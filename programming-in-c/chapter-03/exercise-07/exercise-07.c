ca5d998d701ad9d000eeb19abef27cdcecfd76e9
Justin D. Smith <cinephile85@outlook.com>
2019-02-03 18:06:02 -0500

    Add exercise-07.c

---
+++ programming-in-c/chapter-03/exercise-07/exercise-07.c
@@ -0,0 +1,18 @@
+// Justin D. Smith
+// 3 February 2019
+// Programming in C, Fourth Edition
+// Exercise 3.7
+
+// Evaluate the expression (3.31 x 10⁻⁸ x 2.01 x 10⁻⁷) / (7.16 x 10⁻⁶ + 2.01 x 10⁻⁸)
+// And display the result
+
+#include <stdio.h>
+
+int main(void)
+{
+    //  Evaluate the expression and display the result
+    long double result = (3.31e-8L * 2.01e-7L) / (7.16e-6L + 2.01e-8L);
+    printf("%Le\n", result);
+
+    return 0;
+}

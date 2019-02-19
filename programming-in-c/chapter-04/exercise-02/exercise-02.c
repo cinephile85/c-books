afe35fa0989be868984ec5091b9b7fb027195f32
Justin D. Smith <cinephile85@outlook.com>
2019-02-19 00:33:33 -0500

    Add exercise-02.c

---
+++ programming-in-c/chapter-04/exercise-02/exercise-02.c
@@ -0,0 +1,25 @@
+// Justin D. Smith
+// 19 February 2019
+// Programming in C, Fourth Edition
+// Exercise 4.2
+
+/* A program to generate and display a table of n and n²,
+   for integer values of n ranging from 1 to 10           */
+
+#include <stdio.h>
+
+int main (void)
+{
+    int n, n_squared;
+
+    printf ("TABLE OF VALUES FOR n AND n²\n\n");
+    printf (" n           n²\n");
+    printf ("---        -----\n");
+
+    for ( n = 1; n <= 10; ++n )
+        printf ("%2i          %i\n", n, n * n);
+
+    printf ("\n");
+
+    return 0;
+}

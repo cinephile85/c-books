28b7f5c08904f1b6bf7800f52b7218096c0b2dc5
Justin D. Smith <cinephile85@outlook.com>
2019-02-23 19:55:31 -0500

    Add exercise-03.c

---
+++ programming-in-c/chapter-04/exercise-03/exercise-03.c
@@ -0,0 +1,29 @@
+// Justin D. Smith
+// 23 February 2019
+// Programming in C, Fourth Edition
+// Exercise 4.3
+
+/* A program to generate and display a table of every
+   fifth triangular number between 5 and 50           */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int n, triangularNumber;
+
+  printf("TABLE OF FIFTH TRIANGULAR NUMBERS BETWEEN 5 AND 50\n\n");
+  printf(" n           Triangular Number\n");
+  printf("---          -----------------\n");
+
+  // Calculate and display every fifth triangular number between 5 and 50
+  for (n = 5; n <= 50; n += 5)
+  {
+    triangularNumber = (n * (n + 1)) / 2;
+    printf("%2i                  %i\n", n, triangularNumber);
+  }
+
+  printf("\n");
+
+  return 0;
+}

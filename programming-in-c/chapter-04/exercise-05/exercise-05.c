d2d6c3aadd8f541c94f8076c6469074b11ebb2e3
Justin D. Smith <cinephile85@outlook.com>
2019-02-23 21:16:52 -0500

    Add exercise-05.c

---
+++ programming-in-c/chapter-04/exercise-05/exercise-05.c
@@ -0,0 +1,25 @@
+// Justin D. Smith
+// February 23 2019
+// Programming in C, Fourth Edition
+// Exercise 4.5
+
+#include <stdio.h>
+
+int main(void)
+{
+  int n, two_to_the_n;
+
+  printf("TABLE OF POWERS OF TWO\n\n");
+  printf(" n     2 to the n\n");
+  printf("---    --------------\n");
+
+  two_to_the_n = 1;
+
+  for (n = 0; n <= 10; ++n)
+  {
+    printf("%2i        %i\n", n, two_to_the_n);
+    two_to_the_n *= 2;
+  }
+
+  return 0;
+}
